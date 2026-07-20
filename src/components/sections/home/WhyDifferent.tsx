b8c12c462d7088b535bad3e622382626ecaf5b5a
Paboda Karunaratne <paboda02@gmail.com>
2026-07-20 23:52:01 +0530

    reduced padding top and bottom

--- src/components/sections/home/WhyDifferent.tsx
+++ src/components/sections/home/WhyDifferent.tsx
@@ -15,7 +15,7 @@ const features = [
 
 export default function WhyDifferent() {
   return (
-    <section className="flex flex-col items-center justify-center gap-10 bg-bg-deep py-20 lg:min-h-[703px] lg:gap-12 lg:py-16">
+    <section className="flex flex-col items-center justify-center gap-8 bg-bg-deep py-12 lg:min-h-[620px] lg:gap-10 lg:py-10">
       <Container className="lg:pt-[55px]">
         <div className="grid grid-cols-1 gap-x-[58px] gap-y-12 lg:grid-cols-[440px_1fr]">
           <div className="flex flex-col items-start gap-[30px]">
